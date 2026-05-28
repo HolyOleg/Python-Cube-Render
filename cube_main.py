753ecb2595bfa7a8b230ad82a6288d7e2f6f237d
HolyOleg <michail.v.fedorov@gmail.com>
2026-05-28 17:22:28 +0400

    Adding some prrettiness

--- cube_main.py
+++ cube_main.py
@@ -2,146 +2,186 @@ import sys
 import math
 import pygame
 
-#VARIABLES YOU CAN CHANGE
-tickrate=200
-window_side_size=1000 #WINDOW IS SQUARED
-rotation_speed=1
-color_speed=2
-moving_speed=1
+# CONSTANTS YOU CAN CHANGE
+TICKRATE = 200
+WINDOW_SIDE_SIZE = 1000  # WINDOW IS SQUARED
+ROTATION_SPEED = 1
+COLOR_SPEED = 2
+MOVING_SPEED = 1
+WINDOW_TITLE = "Cube"
 
-#CONSTANTS
-dx=window_side_size//2
-dy=window_side_size//2
-radian=math.radians(rotation_speed)
-
-#INITIALIZATION PART
-coordinates=[]
-rainbow_red=0
-rainbow_green=0
-rainbow_blue=0
-fun=False
-pixel_set=set()
+# INITIALIZATION PART
+dx = WINDOW_SIDE_SIZE // 2
+dy = WINDOW_SIDE_SIZE // 2
+cube_coordinates = []
+rainbow_red = 0
+rainbow_green = 0
+rainbow_blue = 0
+fun = False
+pixel_set = set()
+d_angle = math.radians(ROTATION_SPEED)
 
 pygame.init()
-screen = pygame.display.set_mode((window_side_size, window_side_size))
-pygame.display.set_caption("Cube")
-window_title = 'Cube'
+screen = pygame.display.set_mode((WINDOW_SIDE_SIZE, WINDOW_SIDE_SIZE))
+pygame.display.set_caption(WINDOW_TITLE)
 
-#CREATING ALL DOTS FOR THE CUBE
-for x in 1,0:
-    for y in 1,0:
-        for z in 1,0:
-            coordinates.append((-100+x*200,-100+y*200,-100+z*200))
+# CREATING ALL DOTS FOR THE CUBE
+for cube_x in 1, 0:
+    for cube_y in 1, 0:
+        for cube_z in 1, 0:
+            cube_coordinates.append((-100 + cube_x * 200, -100 + cube_y * 200, -100 + cube_z * 200))
 
-def draw_line(point1,point2):
-    x1,y1,z1=point1
-    x2,y2,z2=point2
-    angle=math.atan2(x2-x1,y2-y1)
-    length=int(math.sqrt((x2-x1)**2+(y2-y1)**2))
-    if length==0:
+
+def x_matrix_transformation(point, angle):
+    x, y, z = point[0], point[1], point[2]
+    return (
+        x,
+        z * math.sin(angle) + y * math.cos(angle),
+        z * math.cos(angle) - y * math.sin(angle),
+    )
+
+
+def y_matrix_transformation(point, angle):
+    x, y, z = point[0], point[1], point[2]
+    return (
+        x * math.cos(angle) + z * math.sin(angle),
+        y,
+        z * math.cos(angle) - x * math.sin(angle),
+    )
+
+
+def z_matrix_transformation(point, angle):
+    x, y, z = point[0], point[1], point[2]
+    return (
+        x * math.cos(angle) + y * math.sin(angle),
+        x * (-1) * math.sin(angle) + y * math.cos(angle),
+        z,
+    )
+
+
+def draw_line(point1, point2):
+    x1, y1, z1 = point1
+    x2, y2, z2 = point2
+    angle = math.atan2(x2 - x1, y2 - y1)
+    length = int(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
+    if length == 0:
         return
-    for d in range(1,length+1):
-        x=int(x1+d * math.sin(angle))
-        y=int(y1+d * math.cos(angle))
-        t = d / length
-        z = z1 + (z2 - z1) * t
-        brightness = max(0.0, min(1.0, (z + 200) / 300))
-        if fun:
-            r = int(RAINBOW[0] * brightness)
-            g = int(RAINBOW[1] * brightness)
-            b = int(RAINBOW[2] * brightness)
-            pixel_set.add(((x, y), (r, g, b)))
-        else:
-            c = int(255 * brightness)
-            pixel_set.add(((x, y), (c, c, c)))
+    for d in range(1, length + 1):
+        x = x1 + d * math.sin(angle)
+        y = y1 + d * math.cos(angle)
+        drawen_part = d / length
+        z = z1 + (z2 - z1) * drawen_part
+        set_pixel(x, y, z)
 
-print('Rotate on WASD. Move on ARROWS. OLEG for fun')
+
+def set_pixel(x, y, z):
+    brightness = max(0.0, min(1.0, (z + 200) / 300))
+    if fun:
+        r = int(rainbow[0] * brightness)
+        g = int(rainbow[1] * brightness)
+        b = int(rainbow[2] * brightness)
+        pixel_set.add(((int(x), int(y)), (r, g, b)))
+    else:
+        c = int(255 * brightness)
+        pixel_set.add(((int(x), int(y)), (c, c, c)))
+
+
+print("Rotate on WASD. Move on ARROWS. OLEG for fun")
 
 clock = pygame.time.Clock()
 
-#MAIN PART
+# MAIN PART
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
-    #UPDATING SCRIPT
+
+    # UPDATING SCRIPT
     pygame.display.flip()
-    screen.fill((0,0,0))
-    
-    #KEY DETECTING PART
+    screen.fill((0, 0, 0))
+
+    # KEY DETECTING PART
     keys = pygame.key.get_pressed()
 
     ##MOVING
     if keys[pygame.K_LEFT]:
-        dx-=moving_speed
+        dx -= MOVING_SPEED
     if keys[pygame.K_UP]:
-        dy-=moving_speed
+        dy -= MOVING_SPEED
     if keys[pygame.K_DOWN]:
-        dy+=moving_speed
+        dy += MOVING_SPEED
     if keys[pygame.K_RIGHT]:
-        dx+=moving_speed
+        dx += MOVING_SPEED
 
     ##FUN ACTIVATION
     if keys[pygame.K_o]:
         if keys[pygame.K_l]:
             if keys[pygame.K_e]:
                 if keys[pygame.K_g]:
-                    fun=True
-    
+                    fun = True
+
     ##ROTATING
-    for dot_index in range(len(coordinates)):
-        point=coordinates[dot_index]
+    for index, _ in enumerate(cube_coordinates):
+        cube_point = cube_coordinates[index]
         ###Z-AXIS
         if keys[pygame.K_d]:
-            point=(point[0]*math.cos(radian)-point[1]*math.sin(radian),
-                 point[0]*math.sin(radian)+point[1]*math.cos(radian),
-                 point[2])
+            cube_point = z_matrix_transformation(cube_point,d_angle*(-1))
         if keys[pygame.K_a]:
-            point=(point[0]*math.cos(radian)+point[1]*math.sin(radian),
-                 point[0]*math.sin(radian)*(-1)+point[1]*math.cos(radian),
-                 point[2])
+            cube_point = z_matrix_transformation(cube_point,d_angle)
         ###X-AXIS
-        if keys[pygame.K_s]:
-            point=(point[0],
-                 point[1]*math.cos(radian)+point[2]*math.sin(radian),
-                 point[1]*math.sin(radian)*(-1)+point[2]*math.cos(radian))
         if keys[pygame.K_w]:
-            point=(point[0],
-                 point[1]*math.cos(radian)-point[2]*math.sin(radian),
-                 point[1]*math.sin(radian)+point[2]*math.cos(radian))
-        ###Z-AXIS
+            cube_point = x_matrix_transformation(cube_point,d_angle*(-1))
+        if keys[pygame.K_s]:
+            cube_point = x_matrix_transformation(cube_point,d_angle)
+        ###Y-AXIS
         if keys[pygame.K_q]:
-            point=(point[0]*math.cos(radian)-point[2]*math.sin(radian),
-                point[1],
-                point[0]*math.sin(radian)+point[2]*math.cos(radian))
+            cube_point = y_matrix_transformation(cube_point,d_angle*(-1))
         if keys[pygame.K_e]:
-            point=(point[0]*math.cos(radian)+point[2]*math.sin(radian),
-                point[1],
-                point[0]*math.sin(radian)*(-1)+point[2]*math.cos(radian))
-        coordinates[dot_index]=point
+            cube_point = y_matrix_transformation(cube_point,d_angle)
+        cube_coordinates[index] = cube_point
 
-    #FUN COLOR
-    RAINBOW= (
-        int(127.5*math.sin(rainbow_red/127.5)+127.5),
-        int(127.5*math.sin(rainbow_green/127.5-42.5)+127.5),
-        int(127.5*math.sin(rainbow_blue/127.5+42.5)+127.5))
-    rainbow_red=(rainbow_red+color_speed)%(math.pi*255)
-    rainbow_green=(rainbow_green+color_speed)%(math.pi*255)
-    rainbow_blue=(rainbow_blue+color_speed)%(math.pi*255)
+    # FUN COLOR
+    rainbow = (
+        int(127.5 * math.sin(rainbow_red / 127.5) + 127.5),
+        int(127.5 * math.sin(rainbow_green / 127.5 - 42.5) + 127.5),
+        int(127.5 * math.sin(rainbow_blue / 127.5 + 42.5) + 127.5),
+    )
+    rainbow_red = (rainbow_red + COLOR_SPEED) % (math.pi * 255)
+    rainbow_green = (rainbow_green + COLOR_SPEED) % (math.pi * 255)
+    rainbow_blue = (rainbow_blue + COLOR_SPEED) % (math.pi * 255)
 
-    #DRAWING
-    dots=[]
-    for i in range(len(coordinates)):
-        dots.append((coordinates[i][0]+dx, coordinates[i][1]+dy, coordinates[i][2]))
-    for a,b in ((0,1),(0,2),(0,4),(1,3),(1,5),(2,3),(2,6),(3,7),(4,6),(4,5),(5,7),(6,7)):
-        draw_line(dots[a], dots[b])
+    # DRAWING
+    points = []
+    for cube_point_coordinate in cube_coordinates:
+        points.append(
+            (
+                cube_point_coordinate[0] + dx,
+                cube_point_coordinate[1] + dy,
+                cube_point_coordinate[2],
+            )
+        )
+    for point_x, point_y in (
+        (0, 1),
+        (0, 2),
+        (0, 4),
+        (1, 3),
+        (1, 5),
+        (2, 3),
+        (2, 6),
+        (3, 7),
+        (4, 6),
+        (4, 5),
+        (5, 7),
+        (6, 7),
+        (0, 6),
+        (2, 4),
+    ):
+        draw_line(points[point_x], points[point_y])
 
     sorted_data = sorted(pixel_set, key=lambda item: item[1])
 
-    for point in sorted_data:
-        screen.set_at(point[0],point[1])
+    for cube_point in sorted_data:
+        screen.set_at(cube_point[0], cube_point[1])
     pixel_set.clear()
-
-    clock.tick(tickrate)
+    clock.tick(TICKRATE)
